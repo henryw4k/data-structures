c53d3ecf8be0e20acd0ff9956ce3a6977ad27242
henryw4k <henryw4k@gmail.com>
2014-12-11 12:26:20 -0800

    finish up to set

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,31 +1,52 @@
 
 
 var Graph = function(){
+  this.network = {};
+
 };
 
 Graph.prototype.addNode = function(node){
+
+  this.network[node] = {};
 };
 
 Graph.prototype.contains = function(node){
+
+  return !!this.network[node];
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.network[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  return !!(this.network[fromNode][toNode] &&
+    this.network[toNode][fromNode]);
+
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.network[fromNode][toNode] = toNode;
+  this.network[toNode][fromNode] = fromNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  if(this.hasEdge(fromNode, toNode)){
+    delete this.network[fromNode][toNode];
+    delete this.network[toNode][fromNode];
+  }
 };
 
 Graph.prototype.forEachNode = function(cb){
+  cb = cb || _.identity;
+  _.each(this.network,function(value,key){
+    cb(key);
+  });
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * log(n) and forEachNode linear
  */
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,23 +1,36 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  console.log(LimitedArray);
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i, v);
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
+  var val = this.retrieve(k);
+  this._storage.each(function(asdf){
+    if(val === asdf){
+      delete asdf;
+    }
+  });
 
+  //delete this.retrieve(k);
 };
-
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * LimitedArray: get(index), set(index, value), each(func);
  */
+
+
