398b16bd21c9dd41fc1db5f3aba62c95732bee4f
henryw4k <henryw4k@gmail.com>
2014-12-11 21:19:58 -0800

    finish BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,72 @@
 var BinarySearchTree = function(value){
+  var obj = {};
+  obj.left = null;
+  obj.right = null;
+  obj.value = value;
+
+  obj.insert = function(val, currentNode){
+
+    currentNode = currentNode || obj;
+
+    if(val > currentNode.value){
+      if(currentNode.right === null){
+        currentNode.right = BinarySearchTree(val);
+      } else{
+        currentNode.insert(val, currentNode.right);
+      }
+    }
+    if(val < currentNode.value){
+      if(currentNode.left === null){
+        currentNode.left = BinarySearchTree(val);
+      } else{
+        currentNode.insert(val, currentNode.left);
+      }
+    }
+
+  };
+
+  obj.contains = function(val, currentNode){
+
+    currentNode = currentNode || obj;
+
+    if(currentNode.value === val){
+      return true;
+    } else if(val > currentNode.value){
+      if(currentNode.right === null ){
+        return false;
+      }
+      return currentNode.contains(val, currentNode.right);
+
+
+    } else if(val < currentNode.value){
+      if(currentNode.left === null){
+        return false;
+      }
+      return currentNode.contains(val, currentNode.left);
+    }
+
+
+  };
+
+  obj.depthFirstLog = function(callback, currentNode){
+    currentNode = currentNode || obj;
+
+    callback(currentNode.value);
+    if(currentNode.left !== null){
+      currentNode.depthFirstLog(callback, currentNode.left);
+    }
+
+    if(currentNode.right !== null){
+      currentNode.depthFirstLog(callback, currentNode.right);
+    }
+
+
+  };
+
+
+
+  return obj;
+
 };
 
 
